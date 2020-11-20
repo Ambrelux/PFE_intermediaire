fc2e6300e9c3e28e5dc738f1600ebe5d27899d0d
LeKer29 <theo.kergutuil@esme.fr>
2020-11-20 16:43:17 +0100

    add wave class and multiple wave gestion

---
+++ modelisation_acoustique/Assets/Resources/Scripts/Waves/Wave.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+namespace Resources.Scripts.Waves
+{
+    public class Wave
+    {
+        private Vector3 direction;
+        private Vector3 origin;
+        private Color rayColor;
+        //private RaycastHit hit;
+        private float maxDistance = 100f;
+
+        public Wave(Vector3 _direction, Vector3 _origin)
+        {
+            direction = _direction;
+            origin = _origin;
+            rayColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+
+        public Vector3 Direction
+        {
+            get => direction;
+            set => direction = value;
+        }
+        
+        public Vector3 Origin
+        {
+            get => origin;
+            set => origin = value;
+        }
+        
+        public Color RayColor
+        {
+            get => rayColor;
+            set => rayColor = value;
+        }
+        
+        public float MaxDistance
+        {
+            get => maxDistance;
+            set => maxDistance = value;
+        }
+
+        /*public RaycastHit Hit
+        {
+            get => hit;
+            set => hit = value;
+        }*/
+    }
+    
+}
--- modelisation_acoustique/Assets/Resources/Scripts/Waves/waveBehaviour.cs
+++ modelisation_acoustique/Assets/Resources/Scripts/Waves/waveBehaviour.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Resources.Scripts.Waves;
+using Vector3 = UnityEngine.Vector3;
 
 public class waveBehaviour : MonoBehaviour
 {
@@ -9,24 +13,48 @@ public class waveBehaviour : MonoBehaviour
     public float lineOffset = 0.01f;
     public GameObject raycastObject;
     public float maxDistance = 100f;
-    [SerializeField] private LayerMask layers; 
+    [SerializeField] private LayerMask layers;
+    
+    private List<Wave> wavesList = new List<Wave>();
+    public int nbWaves = 0;
+    public int alpha = 15;
+    
+    private void Start()
+    {
+        initWaves();
+    }
+
     void Update()
     {
-
-        Vector3 direction = transform.forward;
-        Vector3 origin = transform.position + lineOffset * direction;
-        
         for (int i = 1; i <= totalBounce; i++)
         {
-            if (Physics.Raycast(origin, direction, out hit, maxDistance, layers))
+            for (int j = 0; j < wavesList.Count; j++)
             {
-                // Debug.DrawRay(hit.point, Vector3.Reflect(direction.normalized, hit.normal), Color.cyan);  
-                Debug.DrawLine(origin, hit.point, Color.red);
-                direction = Vector3.Reflect(direction.normalized, hit.normal);
-                origin = hit.point + lineOffset * direction;
-                // Debug.DrawRay(origin, hit.distance * direction, Color.blue);
-
+                Vector3 direction = wavesList[j].Direction;
+                Vector3 origin = wavesList[j].Origin;
+                if (Physics.Raycast(origin, direction, out hit, wavesList[j].MaxDistance, layers))
+                {
+                    // Debug.DrawRay(hit.point, Vector3.Reflect(direction.normalized, hit.normal), Color.cyan);  
+                    Debug.DrawLine(origin, hit.point, wavesList[j].RayColor);
+                    direction = Vector3.Reflect(direction.normalized, hit.normal);
+                    origin = hit.point + lineOffset * direction;
+                    // Debug.DrawRay(origin, hit.distance * direction, Color.blue);
+                }                
             }
         }
     }
+
+    void initWaves()
+    {
+        Vector3 direction;
+        Vector3 origin;
+        
+        for (int i =0; i < nbWaves; i++)
+        {
+            direction = new Vector3(0, (alpha * i), 1);
+            origin = transform.position + lineOffset * direction;
+            wavesList.Add(new Wave(direction.normalized, origin));
+        }
+    }
 }
+
