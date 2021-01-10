89277f8be5ba81c25f7f635aacf3bfe15b0d931e
LeKer29 <theo.kergutuil@esme.fr>
2021-01-10 18:37:28 +0100

    update user interface

---
+++ modelisation_acoustique/Assets/Res/Scripts/UserInterface/UiManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Res.Scripts.UserInterface
+{
+    public class UiManager : MonoBehaviour
+    {
+        private void ChangeState()
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(!child.gameObject.activeSelf);
+            }
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+                ChangeState();
+            }
+        }
+    }
+}
